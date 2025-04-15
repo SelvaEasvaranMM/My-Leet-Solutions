df132cc7888bc2c6644f887dde531b135cd19d6d
SelvaEasvaranMM <selvaeasvaranece@gmail.com>
2025-04-15 16:10:51 +0530

    Time: 23 ms (25.61%) | Memory: 57.7 MB (82.74%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        boolean ans[]=new boolean[nums.length];
+        /*boolean ans[]=new boolean[nums.length];
         for(int i:nums){
             if(ans[i]){
                 return i;
@@ -8,6 +8,16 @@ class Solution {
             else{
                 ans[i]=true;
             }
+        }
+        return -1;*/
+        HashSet<Integer> st=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(!st.contains(nums[i])){
+                st.add(nums[i]);
+            }
+            else{
+                return nums[i];
+            } 
         }
         return -1;
     }
