faed46922050026ab7ff764096f4c9b73877b9af
SelvaEasvaranMM <selvaeasvaranece@gmail.com>
2025-04-19 21:17:44 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (11.82%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int b=0,c=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='R'){
+                b++;
+            }
+            else{
+                b--;
+            }
+            if(b==0){
+                c++;
+            }
+        }
+        return c;
+    }
+}
