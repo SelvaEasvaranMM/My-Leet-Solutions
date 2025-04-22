119880f30acc44115e6785c58842582aa85f5b5f
SelvaEasvaranMM <selvaeasvaranece@gmail.com>
2025-04-22 16:25:38 +0530

    Time: 3 ms (16.91%) | Memory: 40.6 MB (45.72%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,12 +1,5 @@
 class Solution {
     public int maximum69Number (int num) {
-        /*String s=(num+"");
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)!='9'){
-                s.charAt(i)='6';
-            }
-        }
-        return Integer.parseInt(s);*/
         return Integer.parseInt((num+"").replaceFirst("6","9"));
     }
 }
