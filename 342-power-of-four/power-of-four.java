6dbe0336fe33801f06dc02977a335de18ddf0d38
SelvaEasvaranMM <selvaeasvaranece@gmail.com>
2025-04-29 21:41:25 +0530

    Time: 1 ms (25.31%) | Memory: 40.9 MB (46.15%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        for(int i=0;i<=19;i++){
+            if(n==Math.pow(4,i)) return true;
+        }
+        return false;
+    }
+}
