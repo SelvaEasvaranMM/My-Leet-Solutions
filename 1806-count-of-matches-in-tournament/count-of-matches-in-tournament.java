00d872d1a9116599d40fa7bacdc9da22339e1a2b
SelvaEasvaranMM <selvaeasvaranece@gmail.com>
2025-05-14 11:32:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (26.49%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numberOfMatches(int n) {
-        //return n-1;
-        int sum=0;
+        return n-1;
+        /*int sum=0;
         while(n!=1){
             if(n%2==0){
                 sum+=n/2;
@@ -12,6 +12,6 @@ class Solution {
                 n=((n-1)/2)+1;
             }
         }
-        return sum;    
+        return sum;*/    
     }
 }
