bd611fa3ac48d9dfb45ca491a895a0b05f3a05ba
SelvaEasvaranMM <selvaeasvaranece@gmail.com>
2025-05-21 10:51:56 +0530

    Time: 1 ms (64.86%) | Memory: 40.8 MB (72.97%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        int temp=n;
+        while(true){
+            int pro=1;
+            int x=temp;
+            while(x!=0){
+                int k=x%10;
+                pro*=k;
+                x/=10;
+            }
+            if(pro%t==0) return temp;
+            temp++;
+        }
+    }
+}
