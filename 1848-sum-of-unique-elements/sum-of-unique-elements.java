ca02e44173f9b2b05c03cae1326d40d3afff510d
SelvaEasvaranMM <selvaeasvaranece@gmail.com>
2025-06-03 17:13:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (34.30%) - LeetSync

---
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int freq[]=new int[101];
+        for(int i:nums){
+            freq[i]++;
+        }
+        int sum=0;
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]==1) sum+=i;
+        }
+        return sum;
+    }
+}
